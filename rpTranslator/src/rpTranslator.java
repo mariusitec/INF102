06f0aad16d6bfb24bd149b1d0c7b9e76a2ac175a
MartinJohannessen <32108516+MartinJohannessen@users.noreply.github.com>
2017-09-22 15:26:39 +0200

    Hei Marius eg har endret koden slik at riktig operator kommer på riktig plass. Det gjør den ikkje hvis man putter operatorene i en stack.

--- rpTranslator/src/rpTranslator.java
+++ rpTranslator/src/rpTranslator.java
@@ -8,30 +8,32 @@ public class rpTranslator {
     public static void main(String[] args) {
 
         StdOut.println("Enter expression in reverse polish form: ");
-        String input = StdIn.readLine();
+        //String input = StdIn.readLine();
+        String input = "1 2 + 3 4 5 + + *";
 
         String[] elements = input.split(" ");
 
-        Stack numbers = new Stack();
-        Stack operators = new Stack();
+        Stack everything = new Stack();
 
         for (int i = 0; i < elements.length; i++) {
 
-            if (elements[i].equals("*") || elements[i].equals("+"))
-                operators.push(elements[i]);
-            else
-                numbers.push(elements[i]);
-
-            if (!operators.empty()) {
-
-                String formatted = "(" + numbers.pop() + operators.pop() + numbers.pop() + ")";
-                numbers.push(formatted);
-
+            if (elements[i].equals("*") || elements[i].equals("+")) {
+                String formatted = ")" + everything.pop() + elements[i] + everything.pop() + "(";
+                everything.push(formatted);
+            } else{
+                everything.push(elements[i]);
             }
         }
-
         StdOut.println("Input formatted as bracketed infix:");
-        StdOut.print(numbers.pop());
 
-    }
+        StringBuilder feilVei = new StringBuilder(everything.pop().toString());
+
+        StdOut.println(feilVei.reverse());
+
+
+        }
+
+
+
+
 }
