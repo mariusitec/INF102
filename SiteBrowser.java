858277882dbc0cf930afa94ab53dccb7e015df91
MartinJohannessen <32108516+MartinJohannessen@users.noreply.github.com>
2017-09-25 17:24:09 +0200

    Update SiteBrowser.java

--- SiteBrowser.java
+++ SiteBrowser.java
@@ -1,6 +1,3 @@
-/**
- * Created by Martin on 22.09.2017.
- */
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -14,7 +11,7 @@ public class SiteBrowser {
 
         Stack past = new Stack();
         Stack future = new Stack();
-        String input = "11\n" +
+        /*String input = "11\n" +
                 "Facebook\n" +
                 "Twitter\n" +
                 "Google\n" +
@@ -26,22 +23,28 @@ public class SiteBrowser {
                 "LinkedIn\n" +
                 "∗back∗\n" +
                 "∗back∗";
-        Scanner scanner = new Scanner(input);
+        Scanner scanner = new Scanner(input);*/
 
+        StdOut.println("Enter number of commands you will use and press 'enter', then write commands separated by enter: ");
+        Scanner scanner = new Scanner(StdIn.readLine());
         int numberOfLines = Integer.parseInt(scanner.nextLine());
+        String lines = "";
+        for (int i=0;i<numberOfLines;i++) {
+            lines += StdIn.readLine()+"\n";
+        }scanner = new Scanner(lines);
 
 
         for (int i = 0; i < numberOfLines; i++) {
             String currentLine = scanner.nextLine();
 
-            if (currentLine.equals("∗back∗")) {
-                if (!past.isEmpty()) {
+            if (currentLine.equals("∗back∗")||currentLine.equals("*back*")) {
+                if (past.size()>1) {
                     future.push(past.pop());
                     StdOut.println(past.peek());
                 } else {
                     StdOut.println("Error: There is no back from here. ");
                 }
-            } else if (currentLine.equals("∗forward∗")) {
+            } else if (currentLine.equals("∗forward∗")||currentLine.equals("*forward*")) {
                 if (!future.isEmpty()) {
                     past.push(future.pop());
                     StdOut.println(past.peek());
