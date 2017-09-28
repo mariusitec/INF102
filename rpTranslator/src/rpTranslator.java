b9cb0f43d43f57ac2551c88426fb464536130097
mariusitec <marius_kleppe@hotmail.com>
2017-09-28 13:27:29 +0200

    Update rpTranslator.java

--- rpTranslator/src/rpTranslator.java
+++ rpTranslator/src/rpTranslator.java
@@ -1,3 +1,12 @@
+/**
+ * Task 1, mandatory assignment 1
+ * INF102
+ *
+ * @author Marius Kleppe Larnoy
+ * @author Martin Skivenesvaag Johannessen
+ *
+ */
+
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -8,24 +17,26 @@ public class rpTranslator {
     public static void main(String[] args) {
 
         StdOut.println("Enter expression in reverse polish form: ");
-        //String input = StdIn.readLine();
-        String input = "1 2 + 3 4 5 + + *";
+        String input = StdIn.readLine();
 
+        //put all elements into an array separated by space
         String[] elements = input.split(" ");
 
         Stack everything = new Stack();
 
+        //the loop runs through the elements array
         for (int i = 0; i < elements.length; i++) {
 
-            if (elements[i].equals("*") || elements[i].equals("+")) {
-                String formatted = ")" + everything.pop() + elements[i] + everything.pop() + "(";
-                everything.push(formatted);
+            if (elements[i].equals("*") || elements[i].equals("+")) {   //checks if the elements is an operator
+                String formatted = ")" + everything.pop() + elements[i] + everything.pop() + "("; //adds formatted elements
+                everything.push(formatted); //pushes the formatted string to the stack
             } else{
                 everything.push(elements[i]);
             }
         }
-        StdOut.println("Input formatted as bracketed infix:");
 
+        StdOut.println("Input formatted as bracketed infix:");
+        //reverses the finished string
         StringBuilder feilVei = new StringBuilder(everything.pop().toString());
 
         StdOut.println(feilVei.reverse());
