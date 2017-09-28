bb8a0c29323224e7a79b05fab4d3412cf8b705f0
MartinJohannessen <32108516+MartinJohannessen@users.noreply.github.com>
2017-09-28 14:27:44 +0200

    Update rpTranslator.java

--- rpTranslator/src/rpTranslator.java
+++ rpTranslator/src/rpTranslator.java
@@ -18,27 +18,29 @@ public class rpTranslator {
 
         StdOut.println("Enter expression in reverse polish form: ");
         String input = StdIn.readLine();
+        //String input = "1 2 + 3 4 5 + + * 6 7 / 8 9 + * -";
 
-        //put all elements into an array separated by space
+        //puts the input RP strings in an array, as separated by empty space(" ")
         String[] elements = input.split(" ");
 
+        //stack that will recieve elementes from elements[] 
         Stack everything = new Stack();
-
-        //the loop runs through the elements array
+        //for loop that will gradually turn Stack everything into only one element when translated
         for (int i = 0; i < elements.length; i++) {
 
-            if (elements[i].equals("*") || elements[i].equals("+")) {   //checks if the elements is an operator
-                String formatted = ")" + everything.pop() + elements[i] + everything.pop() + "("; //adds formatted elements
-                everything.push(formatted); //pushes the formatted string to the stack
+            if (elements[i].equals("*") || elements[i].equals("+")|| elements[i].equals("-")|| elements[i].equals("/")) {
+                //when elements[i] contains an operator, it pops two numbers from everything[] 
+                //and puts the operator between them, and puts it in parenthesis
+                String formatted = ")" + everything.pop() + elements[i] + everything.pop() + "(";
+                everything.push(formatted);
             } else{
                 everything.push(elements[i]);
             }
         }
-
         StdOut.println("Input formatted as bracketed infix:");
-        //reverses the finished string
-        StringBuilder feilVei = new StringBuilder(everything.pop().toString());
 
+        //oops. Since we didn't use variables for the pops in the for loop, so we have to reverse the string using stringbuilder. 
+        StringBuilder feilVei = new StringBuilder(everything.pop().toString());
         StdOut.println(feilVei.reverse());
 
 
